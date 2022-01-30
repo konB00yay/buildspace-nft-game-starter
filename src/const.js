4d572f2c9f63ec8952a441e7809ffb12f50731d8
Konnor Beaulier <konbeaulier@gmail.com>
2022-01-30 13:50:59 -0800

    tutorial complete

---
+++ src/const.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x6BeC4EFFF6fBB99FF5f6430B44fF5a4d7DC79207';
+
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+export { CONTRACT_ADDRESS, transformCharacterData };
